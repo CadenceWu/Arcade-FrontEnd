31395fc297ee5b54d72248446e49d53ef6a2ac4c
CadenceWu <90173356+CadenceWu@users.noreply.github.com>
2024-12-28 13:30:30 -0800

    Update all modified files

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,13 +1,11 @@
 import React from 'react';
-import { useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import './HomePage.css';
 import arcadeImg from './Arcade.jpg'
 import gameCard from './Card/GameCard.svg'
 import PlayGame from './PlayGame.svg'
 
 const HomePage = () => {
-  const navigate = useNavigate();
-
   const MenuSection = ({ title, children }) => (
     <div className="menu-section">
       <h2 className="menu-title">{title}</h2>
@@ -15,10 +13,10 @@ const HomePage = () => {
     </div>
   );
 
-  const PrimaryButton = ({ onClick, children, className = "" }) => (
-    <button className={`primary-button ${className}`} onClick={onClick}>
+  const PrimaryButton = ({ to, children, className = "" }) => (
+    <Link to={to} className={`primary-button ${className}`}>
       {children}
-    </button>
+    </Link>
   );
 
   return (
@@ -29,36 +27,36 @@ const HomePage = () => {
 
       <div className="home-content">
         <MenuSection title="系統管理">
-          <PrimaryButton onClick={() => navigate('/games')}>
+          <PrimaryButton to="/games">
             遊戲設定
           </PrimaryButton>
-          <PrimaryButton onClick={() => navigate('/prizes')}>
+          <PrimaryButton to="/prizes">
             獎品設定
           </PrimaryButton>
-          <img src={arcadeImg} className="arcade-image"></img>
+          <img src={arcadeImg} className="arcade-image" alt="Arcade" />
         </MenuSection>
 
         <MenuSection title="遊戲卡設定">
-          <PrimaryButton onClick={() => navigate('/cards')}>
-           新增遊戲卡、儲值
+          <PrimaryButton to="/cards">
+            新增遊戲卡、儲值
           </PrimaryButton>
-          <PrimaryButton onClick={() => navigate('/transfer')}>
+          <PrimaryButton to="/transfer">
             轉換代碼、票券
           </PrimaryButton>
-          <PrimaryButton onClick={() => navigate('/check-balance')}>
+          <PrimaryButton to="/check-balance">
             查看卡片餘額
           </PrimaryButton>
-          <img src={gameCard} className="arcade-image"></img>
+          <img src={gameCard} className="arcade-image" alt="Game Card" />
         </MenuSection>
 
         <MenuSection title="遊戲操作、獎品兌換">
-          <PrimaryButton onClick={() => navigate('/playGame')}>
+          <PrimaryButton to="/playGame">
             選擇遊戲
           </PrimaryButton>
-          <PrimaryButton onClick={() => navigate('/request-prize')}>
+          <PrimaryButton to="/request-prize">
             兌換獎品
           </PrimaryButton>
-          <img src={PlayGame} className="arcade-image"></img>
+          <img src={PlayGame} className="arcade-image" alt="Play Game" />
         </MenuSection>
       </div>
     </div>
